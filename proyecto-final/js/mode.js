ae0fca6e364a6406d6c2b125a6d09472ab5446e8
unknown <adrianforero@colegioguanenta.edu.co>
2024-04-21 20:32:42 -0500

    creando el diseño de la página principal

---
+++ proyecto-final/js/mode.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    modeSwitch.addEventListener("click", () =>{
+        body.classList.toggle('dark');
+    })

6da1a4f565c2edb8a5fb22febfa2af6ea86dc778
unknown <adrianforero@colegioguanenta.edu.co>
2024-04-21 22:04:36 -0500

    creando el diseño de la página principal

--- proyecto-final/js/script.js
+++ proyecto-final/js/script.js
@@ -5,6 +5,17 @@ const body = document.querySelector("body"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
+    toggle.addEventListener("click", () =>{
+        sidebar.classList.toggle("close");
+    });
+
+
     modeSwitch.addEventListener("click", () =>{
         body.classList.toggle('dark');
-    })
+
+        if(body.classList.contains('dark')){
+            modeText.innerText= "Light Mode";
+        }else{
+            modeText.innerText= "Dark Mode";
+        }
+    });

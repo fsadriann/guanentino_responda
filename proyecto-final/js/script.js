86ebaca2273097f1044d1548037c05878d5d10dd
fsadriann <adrianforero@colegioguanenta.edu.co>
2024-04-26 22:56:07 -0500

    se añadió una función para conservar el modo oscuro en la página web, se está trabajando en una para conservar la barra desplegada en la página

--- proyecto-final/js/script.js
+++ proyecto-final/js/script.js
@@ -5,25 +5,21 @@ const body = document.querySelector("body"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
+searchBtn.addEventListener("click", () =>{
+    sidebar.classList.remove("close");
+});
+
+document.addEventListener("DOMContentLoaded",() => {
     toggle.addEventListener("click", () =>{
         sidebar.classList.toggle("close");
+        
+        searchBtn.addEventListener("click", () =>{
+            sidebar.classList.remove("close");
+        });
     });
-    searchBtn.addEventListener("click", () =>{
-        sidebar.classList.remove("close");
-    });
+});
 
-
-    modeSwitch.addEventListener("click", () =>{
-        body.classList.toggle('dark');
-
-        if(body.classList.contains('dark')){
-            modeText.innerText= "Light Mode";
-        }else{
-            modeText.innerText= "Dark Mode";
-        }
-    });
-
-// active class
+ // active class
 
 const navLinksEls = document.querySelectorAll('nav-link');
 
@@ -32,3 +28,36 @@ navLinksEls.forEach(navLinkEl =>{
         navLinkEl.classList.add(active);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () =>{
+    loadDarkMode()
+    modeSwitch.addEventListener("click", toggleDarkMode);
+})
+
+function toggleDarkMode(){
+    body.classList.toggle('dark');
+
+    if(body.classList.contains('dark')){
+        modeText.innerText= "Light Mode";
+    }else{
+        modeText.innerText= "Dark Mode";
+    }
+    saveDarkMode(body.classList.contains('dark'))
+}
+
+function saveDarkMode(estado){
+    localStorage.setItem("darkMode", estado)
+}
+
+function loadDarkMode() {
+    const savedDarkMode = localStorage.getItem("darkMode") == "true";
+    if(savedDarkMode) {
+        body.classList.add('dark');
+
+        if(body.classList.contains('dark')){
+            modeText.innerText= "Light Mode";
+        }else{
+            modeText.innerText= "Dark Mode";
+        }
+    }
+}

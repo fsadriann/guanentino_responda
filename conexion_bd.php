289328b091f51880021a11999e54d4e957f14542
fsadriann <adrianforero@colegioguanenta.edu.co>
2024-09-17 18:15:03 -0500

    se creo el register y se conecto a la BD

--- conexion_bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion = new mysqli('localhost', 'root','','guanentino','');
-$conexion->set_charset('utf8');
-
-
-?>
--- register.php
+++ register.php
@@ -1,9 +1,21 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$baseDeDatos = "guanentino";
+
+$enlace = mysqli_connect ($servidor, $usuario, $clave, $baseDeDatos);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registrarse</title>
     <link rel="stylesheet" href="styles.css">
     <script src="main.js"></script>
 </head>
@@ -13,17 +25,22 @@
         <p>Vamos a registrarnos.</p>
     </div>
     <div class="login-form">
-        <form action="" method="post">
+        <form action="#" method="post">
+            
+            <input type="text" name="usuario"  class="input-field" placeholder="Usuario" autocomplete="off" required>
+            
+            <input type="text" name="apellido" class="input-field" placeholder="Apellido" autocomplete="off" required>
+            
+            <input type="text" name="nombre" class="input-field" placeholder="Nombre" autocomplete="off" required>
+            
+            <input type="text" name="correo" class="input-field" placeholder="Correo electrónico" autocomplete="off" required>
 
-            <input type="text" id="usuario" name="usuario"  class="input-field" placeholder="Usuario" autocomplete="off" required>
+            <input type="text" name="telefono" class="input-field" placeholder="Numero de telefono" autocomplete="off" required>
+
             
-            <input type="text" id="nombre" name="nombre" class="input-field" placeholder="Nombre" autocomplete="off" required>
+            <input type="text" name="colegio" class="input-field" placeholder="Institución Educativa" autocomplete="off" required>
             
-            <input type="text" id="colegio" name="colegio" class="input-field" placeholder="Institución Educativa" autocomplete="off" required>
-            
-            <input type="text" id="correo" name="correo" class="input-field" placeholder="Correo electrónico" autocomplete="off" required>
-            
-            <input type="password" id="contraseña" name="contraseña" class="input-field" placeholder="Contraseña" autocomplete="off" required>
+            <input type="password" name="clave" class="input-field" placeholder="Contraseña" autocomplete="off" required>
             
             <input type="submit" class="submit-btn" name="registrar" value="Registrarse">
 
@@ -34,3 +51,24 @@
     </div>
 </body>
 </html>
+
+<?php
+
+if(isset($_POST[ 'registrar' ])){
+    $usuario = $_POST ['usuario'];
+    $apellido = $_POST ['apellido'];
+    $nombre = $_POST ['nombre'];
+    $correo = $_POST ['correo'];
+    $telefono = $_POST ['telefono'];
+    $colegio = $_POST ['colegio'];
+    $clave = $_POST ['clave'];
+
+
+    $insertarDatos = "INSERT INTO estudiante VALUES('', '1', '$usuario', '$apellido', '$nombre', '$correo', '$telefono', '$colegio', '$clave')";
+
+    $ejecutarInsertar = mysqli_query ($enlace, $insertarDatos);
+}
+
+
+
+?>

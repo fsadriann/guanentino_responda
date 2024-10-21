386e0e4b7a468e97c1db417a300ebef2a4cd4446
fsadriann <adrianforero@colegioguanenta.edu.co>
2024-10-21 17:40:00 -0500

    hola mundo

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$clave = "";
-$baseDeDatos = "guanentino";
+$servidor = "sql102.infinityfree.com";
+$usuario = "if0_37434993";
+$clave = "1DB06oxSK9";
+$baseDeDatos = "if0_37434993_guanentino";
 
 $conexion = mysqli_connect ($servidor, $usuario, $clave, $baseDeDatos);
 
